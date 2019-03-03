96957a0a4b8cfa51548bc5351be4114956e33da6
Andrew H <ahennesen@gmail.com>
2019-03-03 18:05:32 -0500

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,6 @@
 // game.js
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+// make a number for the user to guess.
+var targetNumber = Math.floor(Math.random() * 120) + 19;
+$("#number-to-guess").text(targetNumber);
+
