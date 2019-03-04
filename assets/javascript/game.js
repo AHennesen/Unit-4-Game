39b50de3205b647f64d686840905300c65451d08
Andrew H <ahennesen@gmail.com>
2019-03-04 17:21:25 -0500

    fixing a lot

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,17 +4,16 @@
 var targetNumber = Math.floor(Math.random() * 120) + 19;
 $("#number-to-guess").text(targetNumber);
 
-// making the counter for number
+
 var counter = 0;
-var lost = 0;
-var win = 0;
+var wins = 0;
+var losses = 0
 
 var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-// assign variable for 
 for (var i = 0; i < 4; i++) {
     var imageCrystal = $("<img>");
     imageCrystal.addClass("crystal-image");
-    imageCrystal.attr("src", "https://i.pinimg.com/originals/58/3d/8c/583d8c0fcc57e936b0a3257f2f3bf52d.jpg");
+    imageCrystal.attr("src", "http://lorempixel.com/output/city-q-c-100-100-10.jpg");
     imageCrystal.attr("data-crystalvalue", numberOptions[i]);
     $("#crystals").append(imageCrystal);
 }
@@ -25,4 +24,22 @@ $(".crystal-image").on("click", function() {
     var crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
     counter += crystalValue;
+    $("#counter-total").text(counter);
 
+    if (counter === targetNumber) {
+        wins++;
+        alert("You win!");
+        $("#wins-total").text(wins);
+        targetNumber = Math.floor(Math.random() * 120) + 19;
+        $("#number-to-guess").text(targetNumber);
+        counter = 0;
+    }
+    else if (counter >= targetNumber) {
+        losses++;
+        alert("You lose!!");
+        $("#losses-total").text(losses);
+        targetNumber = Math.floor(Math.random() * 120) + 19;
+        $("#number-to-guess").text(targetNumber);
+        counter = 0;
+      }
+    });
