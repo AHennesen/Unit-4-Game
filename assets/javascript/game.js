cbb5fbc188f359b6cb39b231206e9388841f011b
Andrew H <ahennesen@gmail.com>
2019-03-04 18:48:25 -0500

    more fixes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,13 +8,16 @@ $("#number-to-guess").text(targetNumber);
 var counter = 0;
 var wins = 0;
 var losses = 0
+var imgs = ["https://i.pinimg.com/originals/58/3d/8c/583d8c0fcc57e936b0a3257f2f3bf52d.jpg",
+"https://wow.zamimg.com/uploads/screenshots/small/69902.jpg",
+"http://farm4.static.flickr.com/3073/3036157673_b51b1c65cb_o.png",
+"https://crystal-information.com/wp-content/uploads/2015/02/citrine-quartz-example-7.jpg",]
 
-var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 for (var i = 0; i < 4; i++) {
     var imageCrystal = $("<img>");
     imageCrystal.addClass("crystal-image");
-    imageCrystal.attr("src", "http://lorempixel.com/output/city-q-c-100-100-10.jpg");
-    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+    imageCrystal.attr("src", imgs[i]);
+    imageCrystal.attr("data-crystalvalue", Math.floor(Math.random() * 12) +1);
     $("#crystals").append(imageCrystal);
 }
 
@@ -28,7 +31,6 @@ $(".crystal-image").on("click", function() {
 
     if (counter === targetNumber) {
         wins++;
-        alert("You win!");
         $("#wins-total").text(wins);
         targetNumber = Math.floor(Math.random() * 120) + 19;
         $("#number-to-guess").text(targetNumber);
@@ -36,7 +38,6 @@ $(".crystal-image").on("click", function() {
     }
     else if (counter >= targetNumber) {
         losses++;
-        alert("You lose!!");
         $("#losses-total").text(losses);
         targetNumber = Math.floor(Math.random() * 120) + 19;
         $("#number-to-guess").text(targetNumber);
